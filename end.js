94147da2ec0e8a313d4cbb8a1cd413b7088ee229
laura-ash <lauraeashmore@gmail.com>
2020-07-26 09:15:40 +0000

    Prevent clicking save button unless username is entered

--- end.js
+++ end.js
@@ -1,6 +1,12 @@
 /* End page */ 
 
 const username = document.getElementById("username");
+const submitScoreClick = document.getElementById("submitScoreClick")
+
+username.addEventListener("change", () => {
+    console.log(username.value);
+    submitScoreClick.disabled = !username.value;
+});
 
 
 saveScore = function(e) {
