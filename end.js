0000d0da3c696cf568b3106a24d68a3f0467b8b3
laura-ash <lauraeashmore@gmail.com>
2020-07-26 10:11:05 +0000

    Save high scores to local storage

--- end.js
+++ end.js
@@ -31,12 +31,18 @@ saveScore = function(e) {
 };
 highScore.push(score);
 
-// Use only top 5 scores for list
+// Use only top 5 scores for list. Add score to array, splice after 5 and then sort in order. 
 
 highScore.sort((a,b) => b.score - a.score)
 
 // Stop after 5th score
 highScore.splice(5);
+
+// Update local storage with high scores, need to convert to JSON so we can save as string in high scores
+localStorage.setItem("highScore",JSON.stringify(highScore));
+
+//Return home
+window.location.assign("index.html");
 
 
 console.log(highScore);
