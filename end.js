ce47a12c47264807734aeb4c73eaf30b7c893851
laura-ash <lauraeashmore@gmail.com>
2020-07-29 18:59:38 +0000

    Moved button out of form

--- end.js
+++ end.js
@@ -1,51 +1,29 @@
-/* End page */ 
+const username = document.getElementById('username');
+const saveScoreBtn = document.getElementById('saveScoreBtn');
+const finalScore = document.getElementById('finalScore');
+const mostRecentScore = localStorage.getItem('mostRecentScore');
 
-const username = document.getElementById("username");
-const submitScoreClick = document.getElementById("submitScoreClick");
-const endScore = document.getElementById("endScore");
+const highScores = JSON.parse(localStorage.getItem('highScores')) || [];
 
-const highScoreLimit = 5;
+const MAX_HIGH_SCORES = 5;
 
-// update score text 
+finalScore.innerText = mostRecentScore;
 
-const lastScore = localStorage.getItem("lastScore");
-endScore.innerText = lastScore;
-
-//convert local storage from string to object
-const highScore = JSON.parse(localStorage.getItem("highScore")) || [];
-console.log(highScore);
-
-username.addEventListener("change", () => {
-    console.log(username.value);
-    submitScoreClick.disabled = !username.value;
+username.addEventListener('keyup', () => {
+    saveScoreBtn.disabled = !username.value;
 });
 
-
-saveScore = function(e) {
-    console.log("Clicked save");
+saveHighScore = (e) => {
     e.preventDefault();
 
     const score = {
-    score: lastScore,
-    name: username.value
+        score: mostRecentScore,
+        name: username.value,
+    };
+    highScores.push(score);
+    highScores.sort((a, b) => b.score - a.score);
+    highScores.splice(5);
+
+    localStorage.setItem('highScores', JSON.stringify(highScores));
+    window.location.assign('/');
 };
-highScore.push(score);
-
-// Use only top 5 scores for list. Add score to array, splice after 5 and then sort in order. 
-
-highScore.sort((a,b) => b.score - a.score)
-
-// Stop after 5th score
-highScore.splice(5);
-
-// Update local storage with high scores, need to convert to JSON so we can save as string in high scores
-localStorage.setItem("highScore",JSON.stringify(highScore));
-
-//Return home
-window.location.assign("index.html");
-
-
-console.log(highScore);
-};
-
-
