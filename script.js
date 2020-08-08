a57266cdab54bea0eb201f370168613ef9e6a08d
laura-ash <lauraeashmore@gmail.com>
2020-08-08 10:10:39 +0000

    Moved all js files into one

--- script.js
+++ script.js
@@ -131,3 +131,73 @@ increaseScore = number => {
 }
 
 //startQuiz();
+
+/* High Score page */ 
+
+// reference scores list
+const scoresList = document.getElementById("scoresList");
+
+//get high scores out of local storage
+const highScores = JSON.parse(localStorage.getItem("highScores")) || [];
+
+console.log(highScores);
+
+// iterate through each high score and add list elements with username and score inside of it
+
+scoresList.innerHTML = highScores
+    .map( score =>{
+    return `<li class="high-score">${score.name} - ${score.score}</li>`;
+})
+.join("");
+
+/* End High Score Page */
+
+/* Final page */
+const username = document.getElementById('username');
+const submitScoreClick = document.getElementById('submitScoreClick');
+const endScore = document.getElementById('endScore');
+const lastScore = localStorage.getItem('lastScore');
+
+//et reference to high scores, pull what is in local storage and convert to JSON, if nothing there then reference empty array
+const highScores = JSON.parse(localStorage.getItem("highScores")) || [] ;
+console.log(highScores);
+
+const maxScores = 5;
+
+endScore.innerText = lastScore;
+
+username.addEventListener("keyup", () => {
+    submitScoreClick.disabled = !username.value;
+    console.log(username.value);
+}); 
+
+saveScore = e => {
+    console.log("clicked save");
+    e.preventDefault();
+  
+    const score = {
+        score: lastScore,
+        name: username.value
+    };
+
+    highScores.push(score); //push through the score to HighScores array
+
+    // sort scores in highest to lowest, by taking the b score and subtracting a score - if b score is higher than a, then put b before a
+    highScores.sort( (a,b) => { 
+        return b.score - a.score;
+    })
+
+    // cut after 5 scores 
+    highScores.splice(5);
+
+    // update local storage with high scores
+
+    localStorage.setItem("highScores", JSON.stringify(highScores));
+    window.location.assign("index.html");
+
+    console.log(highScores);
+
+    
+};
+
+/* End Final page */
